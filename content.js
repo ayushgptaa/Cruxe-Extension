53a777b7f86e449e09a79c621821d60ce4f36a67
Ayush6968 <ayush.gupta.jk@gmail.com>
2021-05-19 00:35:09 +0530

    Added on screen button for direct summary 🚧

--- content.js
+++ content.js
@@ -1,27 +1,43 @@
 // Content Script is the code that executes after the web page is loaded
-var para = document.getElementsByTagName("p");
-var heading = document.getElementsByTagName("h1");
+window.onload = () => {
+	console.log('hi sexy');
+	var aButton = document.createElement('button');
+	console.log(document);
+	aButton.innerHTML = 'click me';
+	aButton.style.background = 'red';
+	aButton.style.height = '100px';
+	aButton.style.width = '100px';
+	aButton.style.position = 'fixed';
+	aButton.style.top = '0';
+	aButton.style.right = '0';
+	aButton.style.zIndex = '1000000000000000';
+	document.body.appendChild(aButton);
+	console.log(aButton);
+};
+
+var para = document.getElementsByTagName('p');
+var heading = document.getElementsByTagName('h1');
 var selectedHeading = heading[0].innerText;
 
-var selectedText = "";
+var selectedText = '';
 for (var i = 1; i < para.length - 1; i++) {
-  selectedText += para[i].innerText;
+	selectedText += para[i].innerText;
 }
 var mes = {
-  text: selectedText,
-  heading: selectedHeading,
+	text: selectedText,
+	heading: selectedHeading,
 };
 chrome.runtime.sendMessage(mes);
 
-window.addEventListener("mouseup", wordSelected);
+window.addEventListener('mouseup', wordSelected);
 
 function wordSelected() {
-  selectedText = window.getSelection().toString().trim();
-  if (selectedText.length > 0 && selectedHeading) {
-    var message = {
-      text: selectedText,
-      heading: selectedHeading,
-    };
-    chrome.runtime.sendMessage(message);
-  }
+	selectedText = window.getSelection().toString().trim();
+	if (selectedText.length > 0 && selectedHeading) {
+		var message = {
+			text: selectedText,
+			heading: selectedHeading,
+		};
+		chrome.runtime.sendMessage(message);
+	}
 }
